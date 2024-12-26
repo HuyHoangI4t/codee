d148a6eff1e5ab29dc913f20d96afb53f5bb64d3
HuyHoangI4t <152394866+HuyHoangI4t@users.noreply.github.com>
2024-12-27 01:07:45 +0700

    .

---
+++ pythontest/qr.py
@@ -0,0 +1,17 @@
+import qrcode
+
+data = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+
+# Tạo QR Code
+qr = qrcode.QRCode(
+    version = 2,  # Kích thước mã QR, 1 là nhỏ nhất (21x21), càng lớn càng chứa được nhiều dữ liệu
+    error_correction=qrcode.constants.ERROR_CORRECT_L,  # Mức độ sửa lỗi
+    box_size = 10,  # Kích thước mỗi ô vuông trong QR
+    border = 2,  # Viền (mặc định là 4)
+)
+qr.add_data(data)
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+
+img.save("qrcode.png")
