a2c6c414dd255087e5d4e6cf8c19494d543ad8bc
HuyHoangI4t <152394866+HuyHoangI4t@users.noreply.github.com>
2024-12-28 23:46:17 +0700

    ...

---
+++ C++/main1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+class Diem {
+private:
+   int x, y;
+
+public:
+   void SetX(int x){
+      this->x = x;
+   }
+   Diem (int x, int y){
+      this->x = x;
+      this->y = y;
+   }
+};
+
+int main(){
+}
